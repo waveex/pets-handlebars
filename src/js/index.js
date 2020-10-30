ed563e7e27cd900d1b244d4a2510aafbe5d4c4bb
Michal <michal.jablonski097@gmail.com>
2020-10-30 12:55:54 +0100

    petsList: mapping items

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,49 @@
 import '../scss/main.scss';
+const petsData = [
+    {
+      name: "Purrsloud",
+      species: "Cat",
+      favFoods: ["wet food", "dry food", "<strong>any</strong> food"],
+      birthYear: 2017,
+      photo: "https://learnwebcode.github.io/json-example/images/cat-2.jpg"
+    },
+    {
+      name: "Barksalot",
+      species: "Dog",
+      birthYear: 2008,
+      photo: "https://learnwebcode.github.io/json-example/images/dog-1.jpg"
+    },
+    {
+      name: "Meowsalot",
+      species: "Cat",
+      favFoods: ["tuna", "catnip", "celery"],
+      birthYear: 2012,
+      photo: "https://learnwebcode.github.io/json-example/images/cat-1.jpg"
+    }
+  ];
+  function age (birthYear) {
 
-// uncomment the lines below to enable PWA
-// import {registerSW} from './pwa.js';
-// registerSW();
+      return new Date().getFullYear() - birthYear;
 
-/* place your code below */
 
-console.log('HELLO 🚀')
+  }
+  function petTemplate(pet) {
+    return `
+    <div class="animal">
+    <img class="pet-photo" src = "${pet.photo}"
+    <h2 class="pet-name">${pet.name}
+     <span class="spiecies">(${pet.species})
+ <p>Age:<strong> ${age(pet.birthYear)}</strong></p>
+     </span></h2>
+
+        </div>
+    `
+  }
+
+document.getElementById("app").innerHTML = `
+<h1 class= "pets-title">pets (${petsData.length}) results</h1>
+${petsData.map(petTemplate).join("  ")}
+<p class= "footer"> These ${petsData.length} were added recently, chceck back soon for updates.
+</p>
+`;
+// backtick - Tamplate litelrals, nie wymaga <br/>
